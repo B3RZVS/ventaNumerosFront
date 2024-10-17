e991ede3c2da34746597cc5dfc5689b886b1442b
B3RZVS <79112396+B3RZVS@users.noreply.github.com>
2024-10-17 09:24:02 -0300

    .

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Esto permite que el servidor escuche en todas las interfaces de red
+    port: 3000, // Cambia esto según el puerto que desees
+  },
 })
